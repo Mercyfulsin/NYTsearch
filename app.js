a199975e3ac860270a72a8f80d4c87ec7fdffeba
Mercyfulsin <aruiz858@gmail.com>
2019-07-23 21:16:10 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+var apiKey = "AAavl15L60Lh1UlShhPgXf3KtttHxcXN";
+var counter = 0;
+var pageNumber = 0;
+var keyword = "programming";
+var apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword
+ + "&api-key=" + apiKey + "&page=" + pageNumber;
+ var resultArray;
+ $(document).ready(function(){
+
+    $("#search").click(function(){
+        keyword = $("#searchParm").val();
+        callAjax();
+    });
+    $("#clear").click(function(){
+        $("#word-blanks").empty();
+    });
+ });
+
+ function callAjax(){
+    $.ajax({
+        url: apiUrl,
+        method:'GET'
+    }).done(function(result){
+        resultArray = result.response.docs;
+        console.log(result.response.docs);
+        for(var i = 0; i < 10; i++){
+           var articleDiv = $("<div>");
+           articleDiv.html("<h5>" + resultArray[i].headline.main + "</h5><br>");
+           articleDiv.attr("url", resultArray[i].web_url);
+
+           articleDiv.click(function(){
+               window.open( $(this).attr("url") ,'_blank');
+           });
+
+           $("#word-blanks").append(articleDiv);
+       }
+    });
+ }
